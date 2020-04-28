78ea5bd643d0a14542a740b19d030b056ba90317
Xavier Perrin <xl.perrin@gmail.com>
2020-04-28 10:49:31 +0200

    getter/setter

--- src/gestibank/User.java
+++ src/gestibank/User.java
@@ -1,11 +1,69 @@
 package gestibank;
 
-public class User {
-private String lastname;
-private String firstname;
-private int id;
-private String mail;
-private String password;
+class User  {
+	
+	private String lastname;
+	private String firstname;
+	private int id;
+	private String mail;
+	private String password;
+	private String gender;
+	
+	
+	
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	
+		public boolean authentification(int id, String password) {
+			return true;
+		}
+	
 
 
 	
