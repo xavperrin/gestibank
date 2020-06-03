279944ec240f82d8b358ebbf33ba83372e16a92f
Xavier <xl.perrin@gmail.com>
2020-06-03 20:58:16 +0200

    Ajout /SuperAdministratorTest.java vide

---
+++ src/test/SuperAdministratorTest.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Jean-Francois
+ *
+ */
+class SuperAdministratorTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link gestibank.user.SuperAdministrator#SuperAdministrator(java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	void testSuperAdministrator() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
