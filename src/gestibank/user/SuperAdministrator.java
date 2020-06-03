22f01b020c6bb5ea6f831f03761fd56ee69c0945
Xavier <xl.perrin@gmail.com>
2020-06-03 20:13:26 +0200

    ajout de SuperAdmin

--- src/gestibank/user/SuperAdministrator.java
+++ src/gestibank/user/SuperAdministrator.java
@@ -4,7 +4,10 @@ public class SuperAdministrator extends User {
 
 	public SuperAdministrator(String id, String firstname, String lastname,  String mail, String password) {
 		super(id, firstname, lastname, mail, password);
-		// TODO Auto-generated constructor stub
+		
 	}
+	
+	
+	
 
 }
